5c0a963c7f09db7c7f06fcd2bb683bd170a9ba12
amossanga600-cpu <amossanga600@gmail.com>
2026-08-12 12:21:00 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,21 @@
-const CACHE_NAME = 'mining-os-v2-2-cache';
+const CACHE_NAME = 'mining-os-v3-cache';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
   'https://cdn.tailwindcss.com',
+  'https://unpkg.com/lucide@latest',
   'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
-
